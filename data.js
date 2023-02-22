aa750cbffe8034e474d80fcf399269d0d28a1089
BeqaGvelesiani <107481738+BeqaGvelesiani@users.noreply.github.com>
2023-02-22 22:30:13 +0400

    Update data.js

--- data.js
+++ data.js
@@ -2,32 +2,32 @@ const staff = {
   1: {
     name: "Bill Mahoney",
     position: "Product owner",
-    avatar: "./images/photo1.png",
+    avatar: "./photo1.png",
   },
   2: {
     name: "Saba Cabrera",
     position: "Art director",
-    avatar: "./images/photo2.png",
+    avatar: "./photo2.png",
   },
   3: {
     name: "Shae Le",
     position: "Tech Lead",
-    avatar: "./images/photo3.png",
+    avatar: "./photo3.png",
   },
   4: {
     name: "Skylah Lu",
     position: "UX Designer",
-    avatar: "./images/photo4.png",
+    avatar: "./photo4.png",
   },
   5: {
     name: "Griff Richards",
     position: "Developer",
-    avatar: "./images/photo5.png",
+    avatar: "./photo5.png",
   },
   6: {
     name: "Stan John",
     position: "DEverloper",
-    avatar: "./images/photo6.png",
+    avatar: "./photo6.png",
   },
 };
 
